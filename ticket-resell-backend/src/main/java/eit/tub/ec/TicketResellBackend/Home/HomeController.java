b3c766e6f5af95d25bf6be349c802e4370803950
Daniel Funke <d.funkeprieto@campus.tu-berlin.de>
2020-01-14 19:08:35 +0100

    Return ResponseEntities in Home endpoints and use constructor to inject dependencies.

--- ticket-resell-backend/src/main/java/eit/tub/ec/TicketResellBackend/Home/HomeController.java
+++ ticket-resell-backend/src/main/java/eit/tub/ec/TicketResellBackend/Home/HomeController.java
@@ -1,34 +1,41 @@
 package eit.tub.ec.TicketResellBackend.Home;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
 @RestController
 public class HomeController {
+    private HomeRepository homeRepository;
 
-    /**
-     * To make use of services and repositories it's enough to annotate the class with "@Autowired". By doing that
-     * it's not necessary to initialize anything, just call the methods of the class.
-     */
-    @Autowired
-    HomeRepository homeRepository;
+    public HomeController(HomeRepository homeRepository) {
+        this.homeRepository = homeRepository;
+    }
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String index() {
-        return "Greetings from Spring Boot!. Check the /homes and /homes/{id} endpoints!";
+    public ResponseEntity<?> index() {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body("Greetings from Spring Boot!. Check the /homes and /homes/{id} endpoints!");
     }
 
     @RequestMapping(value = "/homes", method = RequestMethod.GET)
-    public Iterable<Home> getHomes(@RequestParam(value="id", defaultValue="1") Long id) {
-        Iterable<Home> homes = homeRepository.findAll();
-        return homes;
+    public ResponseEntity<?> getHomes() {
+        return ResponseEntity.status(HttpStatus.OK).body(homeRepository.findAll());
     }
 
     @RequestMapping(value = "/homes/{id}", method = RequestMethod.GET)
-    public Home getHomeById(@PathVariable Long id) {
+    public ResponseEntity<?> getHomeById(@PathVariable Long id) {
         Optional<Home> home = homeRepository.findById(id);
-        return home.orElseGet(Home::new);
+
+        if (home.isPresent()) {
+            return ResponseEntity.status(HttpStatus.OK).body(home.get());
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
     }
 }
