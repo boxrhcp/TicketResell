bdff3615dfae7277fd84e476ef1343383ade9e2d
Spoorthi S <spoorthi.satheesha@campus.tu-berlin.de>
2020-01-21 13:59:19 +0100

    adding user client initial commit

---
+++ ticket-resell-client-user/src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <div v-if="this.$store.state.isLoggedIn !== true" class="container">
+      <HelloWorld />
+    </div>
+    <div v-else class="container-fluid">
+      <HelloWorld />
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
