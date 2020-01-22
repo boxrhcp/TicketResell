70235306ebf84e7b90fc8531626a47c6d2876784
Fayan Leonardo Pardo <fayanpardo@gmail.com>
2020-01-22 18:17:45 +0100

    added new contracts

---
+++ ticket-resell-backend/src/main/resources/ownercontract.sol
@@ -0,0 +1,55 @@
+pragma solidity ^0.5.0;
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/ownership/Ownable.sol";
+import "@openzeppelin/contracts/lifecycle/Pausable.sol";
+import "@openzeppelin-solidity/contracts/lifecycle/Destructible.sol";
+
+contract TicketResell is Ownable, Pausable, Destructible{
+
+    ERC721 public tickets;
+    uint256 public currentPrice;
+
+    event Sent(address indexed owner, uint256 amount, uint256 balance);
+    event Received(address indexed buyer, uint tokenId, uint256 amount, uint256 balance);
+
+    /**
+    *  The owner of the ticket can create this contract sending the address of the tickets contract and the price
+    **/
+    constructor(address tickets_contract_address, uint256 price) public {
+        require(tickets_contract_address != address(0) && tickets_contract_address != address(this));
+        require(price > 0);
+        tickets = ERC721(tickets_contract_address);
+        currentPrice = price;
+    }
+
+    /**
+    *  The buyer can call this function to purchase the ticket of the owner,
+    *  the owner must have approved this contract to sell it.
+    **/
+    function buyTicket(uint256 ticketId) public payable whenNotPaused {
+        require(msg.sender != address(0) && msg.sender != address(this));
+        require(msg.value >= currentPrice);
+        address ticketSeller = tickets.ownerOf(ticketId);
+        tickets.safeTransferFrom(ticketSeller, msg.sender, ticketId);
+        emit Received(msg.sender, ticketId, msg.value, address(this).balance);
+    }
+
+    /**
+    *  Only the owner of this contract can change the price of this contract
+    **/
+    function setCurrentPrice(uint256 newPrice) public onlyOwner {
+        require(newPrice > 0);
+        currentPrice = newPrice;
+    }
+
+    /**
+    *  Only the owner of this contract can withdraw the money of this contract
+    **/
+    function withdraw(address payable owner, uint256 amount) public onlyOwner {
+        require(owner != address(0) && owner != address(this));
+        require(amount > 0 && amount <= address(this).balance);
+        owner.transfer(amount);
+        emit Sent(owner, amount, address(this).balance);
+    }
+
+}
---
+++ ticket-resell-backend/src/main/resources/tickets.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.5.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721Full.sol";
+import "@openzeppelin/contracts/drafts/Counters.sol";
+
+contract Tickets is ERC721Full {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721Full("Tickets", "TCK") public {
+    }
+
+    /**
+    *  The caller of this function creates a ticket in this contract. This ticket will get a unique ID and it will set
+    *  the caller as owner of the ticket. It expects a URI that will retrieve the metadata information of the ticket.
+    **/
+    function createTicket(string memory tokenURI) public returns (uint256) {
+        _tokenIds.increment();
+
+        uint256 newTicketId = _tokenIds.current();
+        _mint(msg.sender, newTicketId);
+        _setTokenURI(newTicketId, tokenURI);
+
+        return newTicketId;
+    }
+}
