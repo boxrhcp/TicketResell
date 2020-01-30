e22fbd45beb9bf06f695aa5d93c0cde2c9e230c8
Daniel Funke <d.funkeprieto@campus.tu-berlin.de>
2020-01-31 00:31:58 +0100

    Log incoming requests

--- ticket-resell-backend/src/main/java/eit/tub/ec/TicketResellBackend/TicketResellBackendApplication.java
+++ ticket-resell-backend/src/main/java/eit/tub/ec/TicketResellBackend/TicketResellBackendApplication.java
@@ -3,6 +3,7 @@ package eit.tub.ec.TicketResellBackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -25,4 +26,14 @@ public class TicketResellBackendApplication {
 		};
 	}
 
+	@Bean
+	public CommonsRequestLoggingFilter logFilter() {
+		CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+		filter.setIncludeQueryString(true);
+		filter.setIncludePayload(true);
+		filter.setMaxPayloadLength(10000);
+		filter.setIncludeHeaders(false);
+		filter.setAfterMessagePrefix("REQUEST DATA : ");
+		return filter;
+	}
 }
