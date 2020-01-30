249f18317291eae29ca74dd01ce4d549aa919279
Daniel Funke <d.funkeprieto@campus.tu-berlin.de>
2020-01-30 16:06:40 +0100

    Ignore ticket fields that should not be updated when processing a PATCH request

--- ticket-resell-backend/src/main/java/eit/tub/ec/TicketResellBackend/Utils/APIPatch.java
+++ ticket-resell-backend/src/main/java/eit/tub/ec/TicketResellBackend/Utils/APIPatch.java
@@ -3,16 +3,17 @@ package eit.tub.ec.TicketResellBackend.Utils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.BeanWrapper;
 import org.springframework.beans.BeanWrapperImpl;
-import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 /**
- * Implementation copied from https://codippa.com/skip-null-properties-spring-beanutils/
+ * Implementation partly copied from https://codippa.com/skip-null-properties-spring-beanutils/
  */
 
 public class APIPatch {
+    private static String[] ignoredProperties = {"id", "eventId", "ownerId", "ethUri", "ethId", "sellContractId"};
 
     public static void merge(Object source, Object destination){
         BeanUtils.copyProperties(source, destination, getNullPropertyNames(source));
@@ -27,6 +28,8 @@ public class APIPatch {
             Object srcValue = src.getPropertyValue(pd.getName());
             if (srcValue == null) emptyNames.add(pd.getName());
         }
+
+        emptyNames.addAll(Arrays.asList(ignoredProperties));
 
         return emptyNames.toArray(new String[0]);
     }
